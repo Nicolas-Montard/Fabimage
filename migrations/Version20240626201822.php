e814781bcaaa65691f94ad6c8f9dfba4548f6520
Nicolas Montard <montardnico@gmail.com>
2024-06-26 23:23:36 +0200

    fix composer.lock

--- migrations/Version20240626201822.php
+++ migrations/Version20240626201822.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240625151324 extends AbstractMigration
+final class Version20240626201822 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,16 @@ final class Version20240625151324 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE follow_up_email (id INT AUTO_INCREMENT NOT NULL, book_id INT NOT NULL, content_fr LONGTEXT DEFAULT NULL, content_es LONGTEXT DEFAULT NULL, content_et LONGTEXT DEFAULT NULL, send_after INT NOT NULL, subject_fr VARCHAR(255) DEFAULT NULL, subject_es VARCHAR(255) DEFAULT NULL, subject_et VARCHAR(255) DEFAULT NULL, INDEX IDX_7AADE72916A2B381 (book_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE follow_up_email ADD CONSTRAINT FK_7AADE72916A2B381 FOREIGN KEY (book_id) REFERENCES book (id)');
         $this->addSql('ALTER TABLE book DROP follow_up_email_fr, DROP follow_up_email_es, DROP follow_up_email_et');
-        $this->addSql('ALTER TABLE follow_up_email ADD subject_fr VARCHAR(255) DEFAULT NULL, ADD subject_es VARCHAR(255) DEFAULT NULL, ADD subject_et VARCHAR(255) DEFAULT NULL, CHANGE content_fr content_fr LONGTEXT DEFAULT NULL, CHANGE content_es content_es LONGTEXT DEFAULT NULL, CHANGE content_et content_et LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE follow_up_email DROP FOREIGN KEY FK_7AADE72916A2B381');
+        $this->addSql('DROP TABLE follow_up_email');
         $this->addSql('ALTER TABLE book ADD follow_up_email_fr LONGTEXT NOT NULL, ADD follow_up_email_es LONGTEXT NOT NULL, ADD follow_up_email_et LONGTEXT NOT NULL');
-        $this->addSql('ALTER TABLE follow_up_email DROP subject_fr, DROP subject_es, DROP subject_et, CHANGE content_fr content_fr LONGTEXT NOT NULL, CHANGE content_es content_es LONGTEXT NOT NULL, CHANGE content_et content_et LONGTEXT NOT NULL');
     }
 }
