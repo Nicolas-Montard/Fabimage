de284d9f6ae27a7fc83b8a2c5b2b9bf4d118c1cb
Nicolas Montard <montardnico@gmail.com>
2024-08-04 22:22:44 +0200

    m

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -8,26 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PresentationController extends AbstractController
 {
-    #[Route('/imageCoaching', name: 'app_image_coaching')]
-    public function coachingImage(): Response
-    {
-        return $this->render('presentation/coachingImage.html.twig', [
-
-        ]);
-    }
-
     #[Route('/fabienne', name: 'app_fabienne')]
     public function fabienne(): Response
     {
         return $this->render('presentation/fabienne.html.twig', [
-
-        ]);
-    }
-
-    #[Route('/company', name: 'app_company')]
-    public function company(): Response
-    {
-        return $this->render('presentation/company.html.twig', [
 
         ]);
     }
